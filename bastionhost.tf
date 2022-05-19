77f488ecd897c25cd8fcd43409da91e7bcf93171
Abd-Elrahman Nabil <95745245+AbdelrahmanNabill@users.noreply.github.com>
2022-05-19 22:36:46 +0200

    Update bastionhost.tf

--- bastionhost.tf
+++ bastionhost.tf
@@ -4,8 +4,8 @@ resource "aws_instance" "bastion" {
   subnet_id = module.network.lab-public-subnet-1
   vpc_security_group_ids = [aws_security_group.first-sg.id]
   associate_public_ip_address = true
-  key_name = aws_key_pair.ssh-key.key_name
-
+  key_name = aws_key_pair.public_key_pair.key_name
+    
   tags = {
     Name = "bastion-host"
   }
